2a06b35351c454f58888d7dec13cd5f9034485f1
felixparey <mysteryparey@gmail.com>
2024-02-16 01:40:20 +0100

    Added Window Spawning and Despawning behaviour, set fixed size to start window

---
+++ DuckRescue/Views/StartScreen.swift
@@ -0,0 +1,18 @@
+//
+//  StartScreen.swift
+//  DuckRescue
+//
+//  Created by Felix Parey on 16/02/24.
+//
+
+import SwiftUI
+
+struct StartScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartScreen()
+}
